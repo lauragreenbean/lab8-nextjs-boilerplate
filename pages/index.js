a7e1367f8099a7c724b3ddbe3accbab68537d95f
lauragreenbean <lmgreen@usc.edu>
2023-03-11 14:34:00 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ export default function Home() {
           </a>
 
           <a
-            href="/About"
+            href="/About.js"
             className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
