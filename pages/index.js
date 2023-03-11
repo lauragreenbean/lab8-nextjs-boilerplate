5e40997950e2a35074bf8e94bece598130bbd0e6
lauragreenbean <lmgreen@usc.edu>
2023-03-11 14:53:44 -0800

    fixing errors

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Link from 'next/link'; 
-
-import Navbar from './navbar'
-import Footer from './footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
