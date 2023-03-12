1b72db7df806b5de84182b147faf424e099e0e4f
lauragreenbean <lmgreen@usc.edu>
2023-03-11 17:27:18 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,20 @@ const inter = Inter({ subsets: ['latin'] })
 
 import 'next/components/NavBar.js'
 
+export default function BasicExample() {
+return(
+  <>
+  
+  <NavBar>
+    <NavBar.Item href="/about">About</NavBar.Item>
+    <NavBar.Item href="/gallery">Gallery</NavBar.Item>
+    <NavBar.Item href="/contact">Contact</NavBar.Item>
+    
+  </NavBar>
+
+  </>
+)
+}
 export default function Home() {
   return (
     <>
