3fb2bf86ff78ae9af2f19932c4cd3d19264ee7e2
lauragreenbean <lmgreen@usc.edu>
2023-03-16 13:21:38 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import NavBar from 'index.js';
 
-import 'next/components/NavBar.js'
+import 'pages/index.js';
 NavBar();
 
 export default function Home() {
