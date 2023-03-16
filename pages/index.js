01697743b490669e99c0d1d6b5657768d3b0d922
lauragreenbean <lmgreen@usc.edu>
2023-03-16 13:19:33 -0700

    Trying components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import NavBar from '@/src/components/NavBar.jsx';
-const inter = Inter({ subsets: ['latin'] })
+import NavBar from 'index.js';
 
 import 'next/components/NavBar.js'
+NavBar();
 
 export default function Home() {
   return (
@@ -129,4 +128,32 @@ export default function Home() {
       </main>
     </>
   )
-}
+};
+
+export function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
